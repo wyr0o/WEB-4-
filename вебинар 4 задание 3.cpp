db4b5516997beec063a7742d889686309a0662eb
wyr0o <brykin-2006@mail.ru>
2025-03-17 21:19:44 +0300

    Add files via upload

---
+++ вебинар 4 задание 3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int num = 0;
+
+	cout << "Введите целое число: ";
+	cin >> num;
+
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << num << " x " << i << " = " << num * i<< endl;
+
+	}
+}
