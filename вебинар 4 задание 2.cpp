d9e0fd1e8f6319a1dfc58ae7e5f4e98ad20b31e0
wyr0o <brykin-2006@mail.ru>
2025-03-17 21:13:14 +0300

    Add files via upload

---
+++ вебинар 4 задание 2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include<Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int num = 1;
+	int summ = 0;
+
+	cout << "Введите целое число: ";
+	cin >> num;
+
+	while (num != 0) 
+	{
+		summ += num % 10;
+		num /= 10;
+	}
+	cout << "сумма цифр: " << summ;
+}
